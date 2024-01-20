9d340df5f7840bb02f9f3e4a872d1bbc6046363e
mvkozak-fe <mvkozak.fe@gmail.com>
2024-01-21 01:57:51 +0300

    feat: add 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+let fileInfo;
+const folderPath = path.join(__dirname, 'secret-folder');
+fs.readdir(folderPath, {withFileTypes: true, encoding: 'utf-8'}, (err, files) => {
+    if (err) throw err;    
+    files.forEach(file => {
+        if (file.isFile()) {
+            const filePath = path.join(folderPath, file.name);
+            fs.stat(filePath, function(err, stats) {
+                if (err) throw err;
+                const filenameAll = path.basename(filePath);
+                let index = filenameAll.indexOf('.');
+                const filename = filenameAll.slice(0, index);
+                const fileExt = path.extname(filePath).slice(1);
+                let fileStat = stats.size;                
+                fileInfo = filename + ' - ' + fileExt + ' - ' + fileStat + 'b';
+                console.log(fileInfo);
+            });  
+        } 
+    })
+})

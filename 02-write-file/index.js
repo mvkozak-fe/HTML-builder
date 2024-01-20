d8a14b74b52c84d500c7aebd8bea20d1f1e93424
mvkozak-fe <mvkozak.fe@gmail.com>
2024-01-20 23:40:15 +0300

    feat: add 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createWriteStream(filePath);
+
+const { stdin, stdout} = require('process');
+
+const rl = readline.createInterface(stdin, stdout);
+
+rl.setPrompt('Enter your data: \n');
+rl.prompt();
+rl.on('line', data => {
+
+    if (data.includes('exit')) {
+        console.log('bye');
+        rl.close();
+    } else {
+        fs.writeFile(filePath, data, function(error) {
+            if (error) throw error;
+        });
+    }
+
+});
+rl.on('SIGINT', () => {
+    console.log('bye');
+    rl.close();
+})

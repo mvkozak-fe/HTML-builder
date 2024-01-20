1cfee76b31384a9ec748559a78c7de214672347e
mvkozak-fe <mvkozak.fe@gmail.com>
2024-01-20 23:50:09 +0300

    fix: add data to previously data

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,22 @@
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
-
 const filePath = path.join(__dirname, 'text.txt');
 const stream = fs.createWriteStream(filePath);
-
 const { stdin, stdout} = require('process');
-
 const rl = readline.createInterface(stdin, stdout);
 
 rl.setPrompt('Enter your data: \n');
 rl.prompt();
 rl.on('line', data => {
-
     if (data.includes('exit')) {
         console.log('bye');
         rl.close();
     } else {
-        fs.writeFile(filePath, data, function(error) {
+        fs.appendFile(filePath, `${data} \n`, function(error) {
             if (error) throw error;
         });
     }
-
 });
 rl.on('SIGINT', () => {
     console.log('bye');

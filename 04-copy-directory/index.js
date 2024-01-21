7d69ce4f725a17771dad20d6f711a1a4461a35f0
mvkozak-fe <mvkozak.fe@gmail.com>
2024-01-21 04:53:36 +0300

    refactor: delete comments

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,41 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const fsPromises = require('fs/promises'); 
-const { error } = require('console');
 const srcFolderPath = path.join(__dirname, 'files');
 const destFolderPath = path.join(__dirname, 'files-copy');
 let dest;
 let src;
 fsPromises.mkdir(destFolderPath).then().catch(function() { 
     console.log('folder is existed'); 
-    // deleteFiles();
-    // copyFiles();    
 }); 
 fs.readdir(destFolderPath, (err, files) => {
     if (err) throw err; 
     files.forEach(file => {
         src = srcFolderPath + '\\' + path.basename(file);
         dest = destFolderPath + '\\' + path.basename(file); 
-        if (dest !==src) {
+        if (dest !== src) {
             fs.unlink(dest, error => {
                 if (error) throw error; 
             })
         }
     })
-
 })
-// function deleteFiles() {
-//     fs.readdir(destFolderPath, (err, files) => {
-//         if (err) throw err; 
-//         files.forEach(file => {
-//             dest = destFolderPath + '\\' + path.basename(file); 
-//             fs.unlink(dest, (error) => {
-//                 if (error) throw error;                
-//             })
-//         })
-//     })
-// }
-
 fs.readdir(srcFolderPath, (err, files) => {
     if (err) throw err; 
     files.forEach(file => {
@@ -47,7 +31,3 @@ fs.readdir(srcFolderPath, (err, files) => {
         });
     })  
 })
-
-
-
-
